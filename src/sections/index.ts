e188929903560d75351071ae92c05e0651e7c1f9
CamiloMaria <camilocraft08@gmail.com>
2024-04-26 19:50:16 -0400

    Update section IDs in About, Education, and Experience sections

--- src/sections/index.ts
+++ src/sections/index.ts
@@ -1,6 +1,5 @@
 export { default as Hero } from './Hero.astro';
 export { default as About } from './About.astro';
 export { default as Experience } from './Experience.astro';
-export { default as Education } from './Education.astro';
 export { default as Projects } from './Projects.astro';
-export {default as Skills } from './Skills.astro';
+export { default as Skills } from './Skills.astro';
