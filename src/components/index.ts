5131778fadbecc132b802e76c48d13b3aba27109
CamiloMaria <camilocraft08@gmail.com>
2024-04-27 23:44:05 -0400

    Update CV with new projects and update section IDs in Projects section

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { default as Section } from './Section.astro';
 export { default as Footer } from './Footer.astro';
 export { default as Header } from './Header.astro';
+export { default as LinkButton } from './LinkButton.astro';
+export { default as ExperienceItem } from './ExperienceItem.astro';
---
+++ src/consts/tags.ts
@@ -0,0 +1,44 @@
+import { AstroIcon, CSharpIcon, NextIcon, ReactIcon, TailwindIcon, TypeScriptIcon, UnityIcon, ViteIcon } from "@/icons";
+
+export const TAGS = {
+	NEXT: {
+		name: "Next.js",
+		class: "bg-black text-white",
+		icon: NextIcon,
+	},
+	TAILWIND: {
+		name: "Tailwind CSS",
+		class: "bg-[#003159] text-white",
+		icon: TailwindIcon,
+	},
+	ASTRO: {
+		name: "Astro",
+		class: "bg-[#0f0f0f] text-white",
+		icon: AstroIcon,
+	},
+    CSHARP: {
+        name: "CSharp",
+        class: "bg-[#239120] text-white",
+        icon: CSharpIcon,
+    },
+    UNITY: {
+        name: "Unity",
+        class: "bg-black text-white",
+        icon: UnityIcon,
+    },
+    REACT: {
+        name: "React",
+        class: "bg-[#61dafb] text-black",
+        icon: ReactIcon,
+    },
+    TYPESCRIPT: {
+        name: "TypeScript",
+        class: "bg-[#007acc] text-white",
+        icon: TypeScriptIcon,
+    },
+    VITE: {
+        name: "Vite",
+        class: "bg-[#646cff] text-white",
+        icon: ViteIcon,
+    },
+}
